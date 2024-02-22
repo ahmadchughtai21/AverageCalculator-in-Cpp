a18aa3f389a267ee25b8cda1ef3d911a6fceff6a
Ahmad Chughtai <ahmadchughtai21@gmail.com>
2024-02-22 16:24:47 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# AverageCalculator-in-Cpp
+ This C++ program calculates the average of three numbers entered by the user.
---
+++ ques11.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float n1, n2, n3;
+    cout << "Enter Number 1 = ";
+    cin >> n1;
+    cout << "Enter Number 2 = ";
+    cin >> n2;
+    cout << "Enter Number 3 = ";
+    cin >> n3;
+
+    cout << "Average of Numbers = " << (n1 + n2 + n3) / 3;
+
+    return 0;
+}
